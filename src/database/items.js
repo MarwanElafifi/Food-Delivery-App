7c788b987f19901c2d5965b9b9aa46889b9b079a
MarwanElafifi <142824851+MarwanElafifi@users.noreply.github.com>
2023-10-14 10:37:35 +0300

    Initial commit

---
+++ src/database/items.js
@@ -0,0 +1,116 @@
+export const COLOURS = {
+    white: '#ffffff',
+    black: '#000000',
+    lightGray: '#B3B4B6',
+    accent: '#FFC231',
+    accentRed: '#FB5D2E',
+    accentPink: '#F96165',
+  };
+  
+  export const Categories = [
+ 
+    {
+      name: 'Pizza',
+      image: require('../database/images/pizza.png'),
+      items: [
+        {
+          name: 'Pepperoni Pizza',
+          weight: 250,
+          rating: '5.0',
+          price: 199,
+          isTopOfTheWeek: true,
+          image: require('../database/images/pizza/pepperonipizza.png'),
+          size: 'Large 14"',
+          crust: 'Thin Crust',
+          delivery: 30,
+          ingredients: [
+            require('../database/images/Tomato.png'),
+            require('../database/images/cheese.png'),
+            require('../database/images/ham.png'),
+            require('../database/images/garlic.png'),
+          ],
+        },
+        {
+          name: 'Plain Cheese Pizza',
+          weight: 300,
+          rating: '4.5',
+          price: 299,
+          isTopOfTheWeek: false,
+          image: require('../database/images/pizza/plaincheesepizza.png'),
+          size: 'Large 16"',
+          crust: 'Thin Cheese',
+          delivery: 25,
+          ingredients: [
+            require('../database/images/ham.png'),
+            require('../database/images/garlic.png'),
+            require('../database/images/cheese.png'),
+            require('../database/images/Tomato.png'),
+          ],
+        },
+        {
+          name: 'Mexican Green Wave',
+          weight: 350,
+          rating: '4.2',
+          price: 499,
+          isTopOfTheWeek: false,
+          image: require('../database/images/pizza/mexicangreenwave.png'),
+          size: 'Large 15"',
+          crust: 'Thin Crust',
+          delivery: 45,
+          ingredients: [
+            require('../database/images/garlic.png'),
+            require('../database/images/cheese.png'),
+            require('../database/images/Tomato.png'),
+            require('../database/images/ham.png'),
+          ],
+        },
+      ],
+    },
+    {
+      name: 'Soft Drinks',
+      image: require('../database/images/softdrinks.png'),
+      items: [
+        {
+          name: 'Coco Cola',
+          weight: 200,
+          rating: '5.0',
+          price: 299,
+          isTopOfTheWeek: true,
+          image: require('../database/images/softdrinks/cocacola.png'),
+          size: 'Medium Glass',
+          crust: 'Small Ice',
+          delivery: 10,
+          ingredients: [require('../database/images/softdrinks/cocacola.png')],
+        },
+        {
+          name: 'Orange Juice',
+          weight: 500,
+          rating: '4.5',
+          price: 199,
+          isTopOfTheWeek: false,
+          image: require('../database/images/softdrinks/orange.png'),
+          size: 'LArge Glass',
+          crust: 'Large Ice',
+          delivery: 8,
+          ingredients: [require('../database/images/softdrinks/orange.png')],
+        },
+        {
+          name: 'Mango Juice',
+          weight: 150,
+          rating: '4.2',
+          price: 99,
+          isTopOfTheWeek: false,
+          image: require('../database/images/softdrinks/mango.png'),
+          size: 'Large Glass',
+          crust: 'Small Ice',
+          delivery: 5,
+          ingredients: [require('../database/images/softdrinks/mango.png')],
+        },
+      ],
+    },
+  
+  {
+    name: 'SeaFood',
+    image: require('../database/images/SeaFood/shrimp-icon.png'),
+  }
+];
